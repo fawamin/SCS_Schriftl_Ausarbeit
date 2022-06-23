2feac11e5d575f8d9fb416187190bdd1cb8b36b7
Steffen H <hill.steffen1@fh-swf.de>
2022-06-23 12:35:17 +0200

    added list with all save Files

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from GameOfLife import GameOfLife
 
+saveFiles = [
+    "Saves/3x3.npy",
+    "Saves/5x5test.npy",
+    "Saves/10x10Glider.npy",
+    "Saves/64x64.npy",
+    "Saves/test.npy"]
+
 gol = GameOfLife()
-gol.setSaveFile("Saves/5x5test.npy")
+gol.setSaveFile(saveFiles[1])
 gol.loadFileData()
 #gol.createNewArea(10,10)
 gol.printToConsole()
