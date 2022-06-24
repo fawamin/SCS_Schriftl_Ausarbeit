6243b2f9197e11c4e6c034f3fafbd22b3d818ebc
Fabian Waltermann <waltermann.fabian@fh-swf.de>
2022-06-24 16:43:51 +0200

    Basic Portrayal of GameoflifeBoard in the Browser

--- Webservice/Frontend/js/Game_of_life.js
+++ Webservice/Frontend/js/Game_of_life.js
@@ -6,21 +6,33 @@ function buildField()
     var canvas = document.getElementById("GOLCanvas");
     var ctx = canvas.getContext("2d");
     //Variables for width and height of Canvas
-    var width = canvas.width;
-    var height = canvas.height;
+    var Canvaswidth = canvas.width;
+    var Canvasheight = canvas.height;
     // Fill canvas with white
-    ctx.fillStyle = "#FFFFFF"; 
+    ctx.fillStyle = "#000000"; 
     let answer = fetch(`http://localhost:8000/gol`, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ Filename: "5x5test" })})
     .then(response =>{
         fetch("http://localhost:8000/gol",{method: 'GET'})
         .then(response => response.json())
-        .then(data => 
-            data.forEach(
-                element => {
-                    console.log(element);
-                     }
-                )
-            );
+        .then(data => {
+            let width = Canvaswidth / data.length
+            let height = Canvasheight / data.length
+            for(let row = 0; row< data.length; row++)
+            {
+                for(let col = 0; col < data[row].length; col++)
+                {
+                    if(data[row][col] == 0)
+                    {
+                        ctx.fillStyle = "#FFFFFF"; 
+                    }
+                    else
+                    {
+                        ctx.fillStyle = "#000000"; 
+                    }
+                    ctx.fillRect(row * width - 2 ,col * height -2 , width -2 , height- 2)
+                }
+            }
+        });
     });
     //row * width - 2 ,col * height -2 , width -2 , height- 2
 }
