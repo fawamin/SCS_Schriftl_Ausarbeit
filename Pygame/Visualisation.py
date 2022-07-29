0dee7e372780a96cc4d5445fec8905478ba0b209
Steffen H <hill.steffen1@fh-swf.de>
2022-07-29 16:04:36 +0200

    first test implementation of the GameofLife visualization in combination with the pygame menu

--- Pygame/Visualisation.py
+++ Pygame/Visualisation.py
@@ -108,6 +108,8 @@ def main():
         settings.WINDOW_HEIGHT,
         mouse_motion_selection=True,
         theme = pygame_menu.themes.THEME_DARK.copy(),
+        columns = 2,
+        rows = [2, 1],
     )
     menuPlay._disable_widget_update_mousepos_mouseselection = True
 
@@ -115,8 +117,27 @@ def main():
                          pygame_menu.events.BACK)
     menuPlay.add.button('Return to main menu', pygame_menu.events.BACK)
 
+    gol = GameOfLife()
+    gol.setSaveFile(settings.SAVE_FILE)
+    gol.loadFileData()
+    #DAYEVENT is event type thats user generated
+    DAYEVENT  = pygame.USEREVENT+1
+    # cyclelenght is the lenght in ms 
+    cyclelenght = 500
+    # Sets a Timer that triggers the DAYEVENT every cyclelenght ms
+    pygame.time.set_timer(DAYEVENT,cyclelenght)
+    running = True
+    #Render Screen once before first Cycle
+    playSurface = pygame.Surface(size = (500, 500))
+    # playSurface.subsurface(left_top = (50, 100), width_height = (400, 400))
+    renderSurface(playSurface, gol.getField())
+
+    playWidget = menuPlay.add.surface(playSurface, selectable = True, selection_effect = pygame_menu.widgets.NoneSelection())
+    playWidget.set_col_row_index(1, 0, 2)
+    print(playWidget.get_col_row_index())
+
     # -------------------------------------------------------------------------
-    # Create menus:About
+    # Create menus: About
     # -------------------------------------------------------------------------
     menuAbout = pygame_menu.Menu(
         settings.TITLE_ABOUT,
@@ -160,10 +181,15 @@ def main():
         for event in events:
             if event.type == pygame.QUIT:
                 exit()
+            if event.type == DAYEVENT:
+                gol.cycleDay()
+                renderSurface(playSurface, gol.getField())
+                playWidget.force_menu_surface_update()
 
         # Main menu
         if menuMain.is_enabled():
-            menuMain.mainloop(surface=screen, fps_limit=settings.FPS)
+            menuMain.update(events)
+            menuMain.draw(screen)
 
         # Flip surface
         pygame.display.flip()
@@ -171,6 +197,23 @@ def main():
     # Close the window and quit.
     pygame.quit()
     sys.exit()
+
+def renderSurface(surface: pygame.Surface, field: np.ndarray):
+    surface.fill(settings.COLOR_PLAYSURFACE_BACKGROUND)
+    surface.set_colorkey(settings.COLOR_PLAYSURFACE_COLORKEY)
+    #surface.set_alpha(128)
+    rows, cols = field.shape
+    # Find Width and Height of each Square
+    width = surface.get_width() / rows
+    height = surface.get_height() / cols
+    for row, col in np.ndindex(field.shape):
+        #Is Cell Dead or Alive?
+        if field[row,col] == 0:
+            colour = settings.COLOR_DEATH
+        elif field[row,col] == 1:
+            colour = settings.COLOR_ALIVE
+        #Draw Cell
+        pygame.draw.rect(surface, colour, (row * width + 1, col * height + 1, width - 2, height - 2), border_radius = 10)
 
 if __name__ == '__main__':
     main()
--- Pygame/settings.py
+++ Pygame/settings.py
@@ -1,7 +1,18 @@
 import pygame
 import pygame_menu
 
-WINDOW_SIZE = (WINDOW_WIDTH:=800, WINDOW_HEIGHT:=600)
+WINDOW_SIZE = {
+    "WGA":(854, 480),
+    "HD": (1280, 720),
+    "FHD": (1920, 1080),
+    "UWFHD": (2560, 1080),
+    "WQHD": (2560, 1440),
+    "UWQHD": (3440, 1440),
+    "UHD": (3840, 2160),
+    "coustum": (1600, 900),
+}["coustum"]
+
+WINDOW_WIDTH, WINDOW_HEIGHT = WINDOW_SIZE
 
 FPS = 60
 
@@ -23,3 +34,15 @@ FONT_SIZE = 20
 COLOR_BACKGROUND = (25, 25, 25)
 COLOR_DEATH = (0,0,0)
 COLOR_ALIVE = (255,255,255)
+
+COLOR_PLAYSURFACE_BACKGROUND = (1, 0, 0)
+# IMPORTANT: If not None, this color needs to be different from other Cell colors
+COLOR_PLAYSURFACE_COLORKEY = COLOR_PLAYSURFACE_BACKGROUND # COLOR_PLAYSURFACE_BACKGROUND | None
+
+SAVE_FILE = [
+    "Saves/3x3.npy",
+    "Saves/5x5test.npy",
+    "Saves/10x10Glider.npy",
+    "Saves/64x64.npy",
+    "Saves/test.npy"
+    ][1]
