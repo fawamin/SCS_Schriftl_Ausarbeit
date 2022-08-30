6f846f92a22c54e24c25c867fe1160f5c7231161
Fabian Waltermann <waltermann.fabian@fh-swf.de>
2022-08-30 15:37:41 +0200

    main.py file creation

--- Pygame/main.py
+++ Pygame/main.py
@@ -1,20 +1,4 @@
-from GameOfLife import GameOfLife
+from Visualisation import DisplayGameOfLifeMenu
 
-saveFiles = [
-    "Saves/3x3.npy",
-    "Saves/5x5test.npy",
-    "Saves/10x10Glider.npy",
-    "Saves/64x64.npy",
-    "Saves/test.npy"]
 
-gol = GameOfLife()
-gol.setSaveFile(saveFiles[1])
-#gol.loadFileData()
-#gol.createNewArea(10,10)
-gol.printToConsole()
-#gol.saveFile("Saves/10x10Glider.npy")
-for i in range(1,5):
-    print(i)
-    gol.cycleDay()
-    gol.printToConsole()
-    print()
+DisplayGameOfLifeMenu()
